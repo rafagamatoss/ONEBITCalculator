eaf6a944456398e9815d3c583f2889d55bff9740
Ráfaga Matos <rmds1@aluno.ifnmg.edu.br>
2023-03-19 23:25:17 -0300

    feat
    implementation of methods.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,34 @@ const currentOperationText = document.querySelector('#current-op')
 const buttons = document.querySelectorAll('#buttons-container button')
 
 class Calculator{
+    constructor(previousOperationText, currentOperationText){
+        this.previousOperationText = previousOperationText
+        this.currentOperationText = currentOperationText
+        this.currentOperation = ''
+    }
 
+    //adiciionando o digito na tela da calculadora
+    addDigit(digit){
+        this.currentOperation = digit
+        this.updateScreen()
+
+    }
+
+    updateScreen(){
+        this.currentOperationText.innerText += this.currentOperation
+    }
 }
 
+const calc = new Calculator(previousOperationText, currentOperationText)
+
 buttons.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const value = e.target.innerText;
-        console.log(value)
+    btn.addEventListener('click', (e) =>{
+        const value = e.target.innerText
+        if(+value >= 0 || value === '.'){
+            calc.addDigit(value)
+            console.log(value)
+        }else{
+            console.log('operação')
+        }
     })
 })
